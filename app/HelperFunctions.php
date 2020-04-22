6fc553c8de41766c2f698cb135b30d4013c9d9e0
mathec-x <40206389+mathec-x@users.noreply.github.com>
2020-04-22 18:17:07 -0300

    Update HelperFunctions.php

--- app/HelperFunctions.php
+++ app/HelperFunctions.php
@@ -10,7 +10,9 @@ function getContext(){
 //usage echo Mask("##.###.###/####-##",$cnpj);
 function StringMask($mask,$str){
 
-    $str = str_replace(" ","",$str);
+	if(!$str) return NULL;
+
+	$str = str_replace(" ","",$str);
 
     for($i=0;$i<strlen($str);$i++){
         $mask[strpos($mask,"#")] = $str[$i];
